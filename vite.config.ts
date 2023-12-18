844db00142a75262c284f88856d17cd87cb1d895
shaxutang <olaysunju@163.com>
2023-12-18 19:27:31 +0800

    feat: disable hash path

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,12 @@ import Unocss from "unocss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), Unocss()],
+  build: {
+    assetsInlineLimit: 0,
+    rollupOptions: {
+      output: {
+        assetFileNames: "[name].[ext]",
+      },
+    },
+  },
 });
